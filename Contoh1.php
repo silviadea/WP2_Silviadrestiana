c41c304c49db58e1210c529aa1621afd045dd308
silviadea <90900590+silviadea@users.noreply.github.com>
2021-09-19 21:59:38 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,23 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama Saya : Silvia Drestiana Agustina
+		
+		nim saya: 12200901
+		
+		jurusan saya : Sistem informasi
+		
+		Saya kuliah di : BSI 
+
+		saya tinggal di daerah : Bogor 
+		
+		Hobby Saya Adalah : membaca novel";
+
+	}
+
+		
+
+}
